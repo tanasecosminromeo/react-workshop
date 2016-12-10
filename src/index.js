c9ec19d7df775f43e662ae8517bcb3d6051e026d
Cosmin-Romeo TANASE <tanasecosminromeo@gmail.com>
2016-12-10 11:49:08 +0200

    Forms n stuff

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class EditComponent extends React.Component {
+
+//Refactor in Webstorm (sper ca si altele) cand dai rename la o clasa/variabila/etc schimba peste tot
+class HeroComponent extends React.Component {
     render() {
         return (
             <div>
                 <div>{this.props.children}</div>
-                <button
-                    onClick={this.props.toggleSuperman}>
-                    Make him {this.props.isSuperman?"mortal":"immortal"}!
-                </button>
-                <input onChange={this.props.changeName}
-                       type="text"
-                       placeholder="Enter your name" />
+                <form onSubmit={this.props.createHero}>
+                    <input required
+                           name="name"
+                           type="text"
+                           placeholder="Enter your name" />
+                    <select name="herotype">
+                        <option value="paladin">Paladin</option>
+                        <option value="barbar">Barbar</option>
+                        <option value="wizzard">Wizzard</option>
+                    </select>
+                    <input name="difficultly" type="number" min={1} max={3} defaultValue={3} />
+
+                    <input type="submit" placeholder="Submit" />
+                </form>
             </div>
         );
     }
@@ -27,30 +36,37 @@ class App extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {isSuperman: true, name: 'Anonymous'}; //State-urile sunt un fel de props-uri locale componentei = Aka variabile locale pentru componenta curenta
-
+        this.state = {
+            name: ''
+        };
     }
 
-    toggleSuperman = (e) => { //Asa e cu arrow function care se binduieste misto
-        this.setState({isSuperman: !this.state.isSuperman});
-    }
+    createHero = (e) => {
+        e.preventDefault();
 
-    changeName = (e) => {
-        let currentName = e.target.value;
+        let hero = e.target;
 
-        this.setState({name: currentName});
+        const heroData = {
+            name: hero.name.value,
+            difficultly: hero.difficultly.value,
+            type: hero.herotype.value,
+            life: Math.floor(100 / hero.difficultly.value)
+        }
+
+        this.setState(heroData);
     }
 
     render() {
+        console.log('heroData', this.state);
+
         return (
             <div>
                 <h1 className="hello">Hello, {this.state.name}</h1>
                 <div>Is {this.state.name} superman? {this.state.isSuperman?"YES":"No :("}</div>
-                <EditComponent
-                    changeName={this.changeName}
-                    toggleSuperman={this.toggleSuperman}
+                <HeroComponent
+                    createHero={this.createHero}
                     isSuperman={this.state.isSuperman}
-                ><h2>Edit Component:</h2></EditComponent>
+                ><h2>Edit Component:</h2></HeroComponent>
             </div>
         );
     }
